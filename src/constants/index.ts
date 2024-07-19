809d3eb0b34319496fdfe4bc5010e190bc22b3eb
viko4ka <viku81@ya.ru>
2024-07-19 21:48:42 +0300

    token

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,19 +1,18 @@
 import axios from "axios";
 
-export const API_URL = 'https://nostrada-kys.ru';
-const token = '785c342441821d6e6e183b495c66103228252fbee1359b27f1dd78afab4ede7b9f0e84fd560ae95f28dbe76f78b25cce26f83a5812f515b85ae433e0befb76093b8aa5ddc352b536639dca646ffa9e043f687d732142c40decc242a6e699a8ce254a2d58a2d785c33c7de63ddc00ecf91ab441debd2e9ae2db0ed4bc3a96f669';
-
+export const API_URL = "https://nostrada-kys.ru";
+const token =
+  "8cb66fdf1102d5404bef9b30ef9a471b22a7e87c9cb339d9b3ed4443540f7c518d69b854215273175db5539407b00b28e1d634d3a79253ada4b98cb795f06480863207c52cc28f4fee96e975f38c82ed0add20af151333ebd775af573a0e5fc5ae82cc690ae5002fcd9fb64098d7a7ca2da4a7c587f7ab0f3bd9f3804fe9f282";
 
 export const axiosInstanse = axios.create({
-    baseURL: API_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export const setAuthorizationHeader = () => {
-    axiosInstanse.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  axiosInstanse.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 };
 
 setAuthorizationHeader();
-
