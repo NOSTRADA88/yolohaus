d7dfa968f09a5d80f489a4e1b53cef0f4bbe7ea0
Nostrada <prikulmax2003@gmail.com>
2024-08-19 13:32:18 +0300

    dependencies update

--- src/page/error/Error.tsx
+++ src/page/error/Error.tsx
@@ -1,4 +1,4 @@
-import { faHouseCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { faHouseCircleExclamation } from "@fortawesome/free-solid-svg-icons/faHouseCircleExclamation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 
